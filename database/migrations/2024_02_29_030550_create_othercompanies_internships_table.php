de2e9511acdb5cce4b77a090a561b97bdd64626f
ibrahemshabanah2022 <160795069+shipooo@users.noreply.github.com>
2024-02-29 05:09:34 +0200

    create migrations for molhemon internship and other compaies internship

---
+++ database/migrations/2024_02_29_030550_create_othercompanies_internships_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('othercompanies_internships', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('experience_needed');
+            $table->enum('career_level', ['Entry Level', 'Mid Level', 'Senior Level', 'Executive']);
+            $table->enum('education_level', ['High School', 'Bachelor Degree', 'Master Degree', 'Ph.D.', 'Not Specified ']);
+            $table->decimal('salary', 10, 2);
+            $table->text('description');
+            $table->text('requirements');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('othercompanies_internships');
+    }
+};

614da5febb201812c9bc93700f9946481f239c71
ibrahemshabanah2022 <160795069+shipooo@users.noreply.github.com>
2024-04-05 05:08:23 +0200

    modify create blade in internship in dashboard

--- resources/views/admin/internships/create.blade.php
+++ resources/views/admin/internships/create.blade.php
@@ -18,8 +18,7 @@
                                     <li class="breadcrumb-item"><a
                                             href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a></li>
                                     <li class="breadcrumb-item"><a
-                                            href="{{ route('coupon.index') }}">{{ __('Coupon List') }}</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">{{ __('Add Coupon') }}</li>
+                                            href="{{ route('coupon.index') }}">{{ __('Add Internships') }}</a></li>
                                 </ul>
                             </nav>
                         </div>
@@ -32,141 +31,44 @@
                         <div class="item-title d-flex justify-content-between">
                             <h2>{{ __('Add Coupon') }}</h2>
                         </div>
-                        <form action="{{ route('coupon.store') }}" method="post" class="form-horizontal">
+                        <form method="POST" action="{{ route('internships.store') }}" class="form-horizontal">
                             @csrf
+                            <label for="title">Title:</label><br>
+                            <input class="form-control" required type="text" id="title" name="title"><br>
 
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('Coupon Code Name') }} <span class="text-danger">*</span></label>
-                                        <input type="text" name="coupon_code_name" value="{{ old('coupon_code_name') }}"
-                                            placeholder="{{ __('Type coupon code name') }}" class="form-control" required>
-                                        @if ($errors->has('coupon_code_name'))
-                                            <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
-                                                {{ $errors->first('coupon_code_name') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('Start Date') }} <span class="text-danger">*</span></label>
-                                        <input type="date" name="start_date" value="{{ old('start_date') }}"
-                                            class="form-control" required>
-                                        @if ($errors->has('start_date'))
-                                            <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
-                                                {{ $errors->first('start_date') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('End Date') }} <span class="text-danger">*</span></label>
-                                        <input type="date" name="end_date" value="{{ old('end_date') }}"
-                                            class="form-control" required>
-                                        @if ($errors->has('end_date'))
-                                            <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
-                                                {{ $errors->first('end_date') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('Coupon Type') }} <span class="text-danger">*</span></label>
-                                        <select name="coupon_type" id="coupon_type" class="form-control" required>
-                                            <option value="">--{{ __('Select Option') }}--</option>
-                                            <option value="1">{{ __('Global') }}</option>
-                                            <option value="2">{{ __('Instructor') }}</option>
-                                            <option value="3">{{ __('Course') }}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
+                            <label for="experience_needed">Experience Needed:</label><br>
+                            <input class="form-control" required type="text" id="experience_needed"
+                                name="experience_needed"><br>
 
+                            <label for="career_level">Career Level:</label><br>
+                            <select id="career_level" name="career_level" class="form-control" required>
+                                <option value="Entry Level">Entry Level</option>
+                                <option value="Mid Level">Mid Level</option>
+                                <option value="Senior Level">Senior Level</option>
+                                <option value="Executive">Executive</option>
+                            </select><br>
 
-                            <div class="row mb-3 d-none" id="course_id_div">
-                                <div class="col-md-12">
-                                    <div class="input__group">
-                                        <label>{{ __('Course') }}</label>
-                                        <select name="course_ids[]" multiple id="course_ids" class="multiple-basic-single">
-                                            @foreach ($courses as $course)
-                                                <option value="{{ $course->id }}">{{ $course->title }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
+                            <label for="education_level">Education Level:</label><br>
+                            <select id="education_level" name="education_level" class="form-control" required>
+                                <option value="High School">High School</option>
+                                <option value="Bachelor Degree">Bachelor Degree</option>
+                                <option value="Master Degree">Master Degree</option>
+                                <option value="Ph.D.">Ph.D.</option>
+                                <option value="Not Specified">Not Specified</option>
+                            </select><br>
 
-                            <div class="row mb-3 d-none" id="instructor_id_div">
-                                <div class="col-md-12">
-                                    <div class="input__group">
-                                        <label for="instructor_ids">{{ __('Instructor') }}</label>
-                                        <select name="instructor_ids[]" multiple id="instructor_ids"
-                                            class="multiple-basic-single">
-                                            @foreach ($instructors as $instructor)
-                                                <option value="{{ $instructor->id }}">{{ $instructor->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
+                            <label for="salary">Salary:</label><br>
+                            <input class="form-control" required type="text" id="salary" name="salary"><br>
 
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>Discount (Percentage %)(ex: 15, 25, 30, 40 ...)<span
-                                                class="text-danger">*</span></label>
-                                        <input type="number" min="0" step="any" name="percentage"
-                                            value="{{ old('percentage') }}" placeholder="%" class="form-control" required>
-                                        @if ($errors->has('percentage'))
-                                            <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
-                                                {{ $errors->first('percentage') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
+                            <label for="description">Description:</label><br>
+                            <textarea id="description" name="description" class="form-control" required></textarea><br>
 
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('Minimum Amount to Apply Coupon') }}<span
-                                                class="text-danger">*</span></label>
-                                        <input type="number" min="0" name="minimum_amount"
-                                            value="{{ old('minimum_amount') }}"
-                                            placeholder="{{ __('Type minimum amount') }}" class="form-control" required>
-                                        @if ($errors->has('minimum_amount'))
-                                            <span class="text-danger"><i class="fas fa-exclamation-triangle"></i>
-                                                {{ $errors->first('minimum_amount') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
+                            <label for="requirements">Requirements:</label><br>
+                            <textarea id="requirements" name="requirements" class="form-control" required></textarea><br>
 
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="input__group text-black">
-                                        <label>{{ __('Status') }} <span class="text-danger">*</span></label>
-                                        <select name="status" id="status" class="form-control">
-                                            <option value="">--{{ __('Select Option') }}--</option>
-                                            <option value="1">{{ __('Active') }}</option>
-                                            <option value="0">{{ __('Deactivated') }}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-12 text-right">
-                                    <button class="btn btn-primary" type="submit">{{ __('Save') }}</button>
-                                </div>
-                            </div>
-
+                            <button class="btn btn-primary" type="submit">Submit</button>
                         </form>
+
                     </div>
                 </div>
             </div>
@@ -175,43 +77,6 @@
     </div>
     <!-- Page content area end -->
     <!-- create.blade.php -->
-
-    <form method="POST" action="{{ route('internships.store') }}">
-        @csrf
-        <label for="title">Title:</label><br>
-        <input type="text" id="title" name="title"><br>
-
-        <label for="experience_needed">Experience Needed:</label><br>
-        <input type="text" id="experience_needed" name="experience_needed"><br>
-
-        <label for="career_level">Career Level:</label><br>
-        <select id="career_level" name="career_level">
-            <option value="Entry Level">Entry Level</option>
-            <option value="Mid Level">Mid Level</option>
-            <option value="Senior Level">Senior Level</option>
-            <option value="Executive">Executive</option>
-        </select><br>
-
-        <label for="education_level">Education Level:</label><br>
-        <select id="education_level" name="education_level">
-            <option value="High School">High School</option>
-            <option value="Bachelor Degree">Bachelor Degree</option>
-            <option value="Master Degree">Master Degree</option>
-            <option value="Ph.D.">Ph.D.</option>
-            <option value="Not Specified">Not Specified</option>
-        </select><br>
-
-        <label for="salary">Salary:</label><br>
-        <input type="text" id="salary" name="salary"><br>
-
-        <label for="description">Description:</label><br>
-        <textarea id="description" name="description"></textarea><br>
-
-        <label for="requirements">Requirements:</label><br>
-        <textarea id="requirements" name="requirements"></textarea><br>
-
-        <button type="submit">Submit</button>
-    </form>
 @endsection
 
 @push('style')

4d8b70f4f854b7f0fcf0dbf7f621778ae889e07f
ibrahemshabanah2022 <160795069+shipooo@users.noreply.github.com>
2024-05-13 07:53:37 +0300

    fix Frequently Ask Questions. section with the right questions and add a pic for this section

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -835,37 +835,66 @@
 
                         <div class="section-title">
                             <h3 class="section-heading">{{ __(get_option('faq_title')) }}</h3>
-                            <p class="section-sub-heading">{{ __(get_option('faq_subtitle')) }}</p>
+                            <p class="section-sub-heading">CHOOSE FROM OUR ONLINE VIDEO COURSES WITH NEW ADDITIONS</p>
                         </div>
 
                         <div class="accordion" id="accordionExample">
-                            @foreach ($faqQuestions as $key => $faqQuestion)
-                                <div class="accordion-item">
-                                    <h2 class="accordion-header" id="heading_{{ $key }}">
-                                        <button
-                                            class="accordion-button font-medium font-18 {{ $key == 0 ? '' : 'collapsed' }}"
-                                            type="button" data-bs-toggle="collapse"
-                                            data-bs-target="#collapse_{{ $key }}"
-                                            aria-expanded="{{ $key == 0 ? 'true' : 'false' }}"
-                                            aria-controls="collapse_{{ $key }}">
-                                            {{ $key + 1 }}. {{ __($faqQuestion->question) }}
-                                        </button>
-                                    </h2>
-                                    <div id="collapse_{{ $key }}"
-                                        class="accordion-collapse collapse {{ $key == 0 ? 'show' : '' }}"
-                                        aria-labelledby="heading_{{ $key }}"
-                                        data-bs-parent="#accordionExample">
-                                        <div class="accordion-body">
-                                            {{ __($faqQuestion->answer) }}
-                                        </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button font-medium font-18" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_1"
+                                        aria-controls="collapse_1">What is the Molhemoon Platform?
+                                    </button>
+                                </h2>
+                                <div id="collapse_1" class="accordion-collapse collapse" aria-labelledby="heading_1"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        Molhemoon is a youth initiative to empower young people and keep pace with digital
+                                        development
+                                        with a complete, secure, web-based training and learning solution that employs a
+                                        simple user interface
                                     </div>
                                 </div>
-                            @endforeach
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button font-medium font-18" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_2"
+                                        aria-controls="collapse_2">Who uses Molhemoon service?
+                                    </button>
+                                </h2>
+                                <div id="collapse_2" class="accordion-collapse collapse" aria-labelledby="heading_2"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        Just about anyone who wants to improve employee skills management, increase
+                                        knowledge acquisition and retention,
+                                        reduce liability through compliance training, or even just complement traditional
+                                        classroom
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button font-medium font-18" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_3"
+                                        aria-controls="collapse_3">Can users view which courses and tests they have taken?
+                                    </button>
+                                </h2>
+                                <div id="collapse_3" class="accordion-collapse collapse" aria-labelledby="heading_3"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        Yes, your users will see their course completion status and check course progress
+                                        and their current results on tests.
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                     <div class="col-md-6 col-lg-6 col-xl-7">
                         <div class="faq-area-right position-relative">
-                            <img src="{{ getImageFile(get_option('faq_image')) }}" alt="faq" class="img-fluid">
+                            <img src="{{ asset('frontend/assets/img/2350619-1.png') }}" alt="faq"
+                                class="img-fluid">
                             <div class="still-no-luck radius-3 bg-white position-absolute">
                                 <h6>{{ __(get_option('faq_image_title')) }}</h6>
                                 <p>{{ __('Then feel free to') }} <a href="{{ route('contact') }}"
